165987f64bf4c9d86eccf18ed4c5c0b9cb2c62bb
Krishna Kant Mishra <krishna.mishra@emc.com>
2015-05-12 16:23:37 +0530

    Merge Sort

--- algorithms/src/main/java/org/cormen/algo/insertion/InsertionSort.java
+++ algorithms/src/main/java/org/cormen/algo/insertion/InsertionSort.java
@@ -1,21 +1,37 @@
 package org.cormen.algo.insertion;
 
 public class InsertionSort {
-	
-	public int [] sort(int [] elements){
 
-		for(int j = 1;j< elements.length; j++){
-			int key = elements[j];
-			int i = j-1;
-			while(i>=0 && elements[i] >= key) {
-				elements[i + 1] = elements[i];
-				i = i - 1;
-			}
-			elements[i+1] = key;
-		}
-		
-		return elements;
-		
-	}
+    public int[] sortAsc(int[] elements) {
+
+        for (int j = 1; j < elements.length; j++) {
+            int key = elements[j];
+            int i = j - 1;
+            while (i >= 0 && elements[i] >= key) {
+                elements[i + 1] = elements[i];
+                i = i - 1;
+            }
+            elements[i + 1] = key;
+        }
+
+        return elements;
+
+    }
+
+    public int[] sortDesc(int[] elements) {
+
+        for (int j = 1; j < elements.length; j++) {
+            int key = elements[j];
+            int i = j - 1;
+            while (i >= 0 && elements[i] <= key) {
+                elements[i + 1] = elements[i];
+                i = i - 1;
+            }
+            elements[i + 1] = key;
+        }
+
+        return elements;
+
+    }
 
 }
